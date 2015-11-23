f20ec1dc2a0635f0e21bfb8b12daec01481ac9be
Sergey <beda67@mail.ru>
2015-11-24 00:08:38 +0300

    Update controllers.js

--- csgo/app/js/controllers.js
+++ csgo/app/js/controllers.js
@@ -9,18 +9,6 @@ phonecatApp.config(['$routeProvider', '$locationProvider', function($routeProvid
         templateUrl:'template/home.html',
         controller:'PhoneListCtrl'
       })
-      .when('/about',{
-        templateUrl:'template/about.html',
-        controller:'AboutCtrl'
-      })
-      .when('/contact',{
-        templateUrl:'template/contact.html',
-        controller:'ContactCtrl'
-      })
-      .when('/phones/:phoneId', {
-        templateUrl:'template/phone-detail.html',
-        controller:'PhoneDetailCtrl'
-      })
       .when('/csgo', {
         templateUrl:'template/csgo.html',
         controller:'CsgoCtrl'
@@ -41,39 +29,12 @@ phonecatApp.filter('checkmark', function() {
   }
 });
 
-phonecatApp.controller('PhoneListCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
-  //console.log('$location.url() - ', $location.url());
-  //console.log('$location.path() - ', $location.path());
-  //console.log('$location.search() - ', $location.search());
-  //console.log('$location.hash() - ', $location.hash());
-
-  $http.get('phones/phones.json').success(function(data, status, headers, config) {
-    $scope.phones = data;
-  });
-
-}]);
 //About Controller
 phonecatApp.controller('AboutCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
 
 }]);
 //Contact Controller
 phonecatApp.controller('ContactCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
-
-}]);
-//Phone Detail Controller
-phonecatApp.controller('PhoneDetailCtrl',['$scope','$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
-  $scope.phoneId = $routeParams.phoneId;
-  var url = 'phones/'+$routeParams.phoneId+'.json';
-
-
-  $http.get(url).success(function(data) {
-    $scope.phone = data;
-    $scope.mainImageUrl = data.images[0];
-  });
-
-  $scope.setImage = function(imageUrl) {
-    $scope.mainImageUrl = imageUrl;
-  }
 
 }]);
 
