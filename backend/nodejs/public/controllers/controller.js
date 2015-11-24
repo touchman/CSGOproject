5be8eda8ce3f45b121f03a0ffaf207c2c243f17b
d35h <daniel.zaru@gmail.com>
2015-11-24 21:10:05 +0100

    CSGOproject-1-angular-integration: Added service 'player-service', renamed 'controller' to 'player-controller' as well as refactored. Refactored 'players.html'

--- backend/nodejs/public/controllers/controller.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
-    $http.get('/players').success(function(response){
-        console.log("I got a data");
-        $scope.players = response;
-    })
-}]);
-
