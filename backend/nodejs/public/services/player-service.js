7ced87566dcc9ea877946f56e42750559547fc81
d35h <daniel.zaru@gmail.com>
2015-11-25 20:35:22 +0100

    Refactored 'player-controller' and 'player-service'

--- backend/nodejs/public/services/player-service.js
+++ backend/nodejs/public/services/player-service.js
@@ -1,23 +1,25 @@
-angular.module('ui.services', [])
-  .service('playerService', ['$http', function($http) {
-	  return {
-		  getPlayers: getPlayers
-	  };
-	  
-	  function getPlayers() {
+(function() {
+	'use strict';
+	
+	angular.module('ui.services', [])
+	  .service('playerService', ['$http', function($http) {
+		  return {
+			  getPlayers: getPlayers
+		  };
 		  
-		  return $http.get('/players')
-		  		.then(getPlayersComplete);
-		  
-		  function getPlayersComplete(response) {
-			  return response.data;
-		  }
-		  
-		  function getPlayersFailed(error) {
-			  console.log('XHR Failed for getPlayers.' + error.data);
-		  }
-		  
-	  };
-	  
-	  
-  }]);
+		  function getPlayers() {
+			  
+			  return $http.get('/players')
+			  		.then(getPlayersComplete);
+			  
+			  function getPlayersComplete(response) {
+				  return response.data;
+			  }
+			  
+			  function getPlayersFailed(error) {
+				  console.log('XHR Failed for getPlayers.' + error.data);
+			  }
+			  
+		  };
+	  }]);
+})();
