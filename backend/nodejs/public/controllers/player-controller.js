93d56240ce178520458bfca96d0e132f825052e5
d35h <daniel.zaru@gmail.com>
2015-11-25 20:40:55 +0100

    Refactored 'player-contoller'

--- backend/nodejs/public/controllers/player-controller.js
+++ backend/nodejs/public/controllers/player-controller.js
@@ -1,17 +1,21 @@
-angular.module('statMaster', ['ui.services'])
-	   .controller('PlayerController', ['playerService', function(playerService) {
-		   	console.log("Hello World from controller");
-		   	var vm = this;
-		   	
-		   	//Sets vm.players the data obtained from REST service
-		   	getPlayers();
-		   	
-		   	/////////////////////
-		   	
-		   	function getPlayers() {
-		   		playerService.getPlayers().then(function(data) {
-					   vm.players = data;
-				});
-		   	}
-
-}]);
+(function() {
+	'use strict';
+	
+	angular.module('statMaster', ['ui.services'])
+		   .controller('PlayerController', ['playerService', PlayerController]);
+	
+			function PlayerController(playerService) {
+			   	var vm = this;
+			   	
+			   	//Sets vm.players the data obtained from REST service
+			   	getPlayers();
+			   	
+			   	/////////////////////
+			   	
+			   	function getPlayers() {
+			   		playerService.getPlayers().then(function(data) {
+						   vm.players = data;
+					});
+			   	}
+			}	   	
+})();
