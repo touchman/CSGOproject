cf9c3407f41b5be81eecd7b4d9af9e0a36f0b6c7
touchman <beda67@mail.ru>
2015-12-03 18:27:45 +0300

    working testing.js udp server and copy it in udpserver.js

--- backend/nodejs/js/udpserver.js
+++ backend/nodejs/js/udpserver.js
@@ -3,6 +3,10 @@ module.exports = {
 
         //res.sendFile(path.join(__dirname + '/public/listen.html'));
 
+
+        var mongojs = require('mongojs');
+        var db2 = mongojs('matches', ['matches']);
+
         res.sendFile('listen.html', {root: './public'});
 
         var cmd = require('./cmd');
@@ -29,6 +33,14 @@ module.exports = {
 
         var players;
 
+        var date = new Date();
+
+        var dataStr = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' at ' +  date.getHours() + ':' + date.getMinutes();
+
+        var map;
+
+        var user = "sally";
+
         var obj = [];
 
         var init = function () {
@@ -36,14 +48,15 @@ module.exports = {
             var length = Object.keys(players).length;
             obj = [];
             var index;
-            for (index = 0; index < length; ++index) {
+            for (index = 0; index < length +1; ++index) {
                 obj.push({
                     id: players[index].fullSteamId,
                     name: players[index].name,
                     kills: 0,
                     deaths: 0
                 });
-
+                obj.push({map : map,
+                    date: dataStr})
             }
         };
 
@@ -51,6 +64,7 @@ module.exports = {
             var msg = message.toString('ascii').slice(5, -1);
             console.log(msg);
             if (msg.indexOf("Match_Start") > -1) {
+                map = msg.substr(msg.indexOf("Match_Start")+17, msg.length - (msg.indexOf("Match_Start")+18));
                 cmd.serverAccess();
                 console.log("start match");
                 init();
@@ -69,46 +83,69 @@ module.exports = {
          console.log(Object.keys(obj).length);
          */
         var write = function (testLog) {
-            var length = Object.keys(obj).length; // == 2
+            var length = Object.keys(obj).length;
 
             if (testLog.indexOf("killed") > -1) {
                 var index;
                 var count = 0;
-                for (index = 0; index < length; ++index) {
+                for (index = 0; index < length-1; ++index) {
 
                     var steam = index; // steamid#
                     if (count == 2) break;              // we need to count only 2 times (kill and dead) and another circles doesn't matter
 
                     if (testLog.indexOf(obj[steam].id) > -1) {
 
-                        if (testLog.indexOf("killed") > -1) {
-                            if (testLog.indexOf("killed") > testLog.indexOf(obj[steam].id)) {
+                        var countCT = (testLog.match(/<CT>/g) || []).length;
+                        var countT = (testLog.match(/<TERRORIST>/g) || []).length;
+
+                        if (testLog.indexOf("killed") > testLog.indexOf(obj[steam].id)) {
+                            if (countCT == 1 || countT == 1) {
                                 obj[steam].kills++;
                                 count++;
-                            } else {
-                                obj[steam].deaths++;
+                            }
+                            if (countCT == 2 || countT == 2) {
+                                obj[steam].kills--;
                                 count++;
                             }
+                        } else {
+                            obj[steam].deaths++;
+                            count++;
                         }
                     }
                 }
-            } else {
-                if (testLog.indexOf("Match_Start") > -1) {
-                    var index2;
-                    for (index2 = 0; index2 < length; ++index2) {
-                        var steam2 = index2;
-                        obj[steam2].kills = 0;
-                        obj[steam2].deaths = 0;
+            } else if (testLog.indexOf("Match_Start") > -1) {
+                var index2;
+                for (index2 = 0; index2 < length-1; ++index2) {
+                    var steam2 = index2;
+                    obj[steam2].kills = 0;
+                    obj[steam2].deaths = 0;
+                }
+            } else if (testLog.indexOf("suicide") > -1) {
+                var i;
+                for (i = 0; i < length-1; ++i) {
+
+                    var steam = i; // steamid#
+                    if (testLog.indexOf(obj[steam].id) > -1) {
+                        obj[steam].kills--;
+                        obj[steam].deaths++;
+                        return;
                     }
                 }
             }
 
-            console.log(obj);
+            //console.log(obj);
 
-            if(testLog.indexOf('scored "16"') > -1)           // write dataOut file only if match end
-            jsonfile.writeFile(fileOut, obj, function (err) {
-                console.error(err)
-            });
+            /*    if (testLog.indexOf('scored "16"') > -1)           // write dataOut file only if match end
+             jsonfile.writeFile(fileOut, obj, function (err) {
+             console.error(err)
+             });*/
+
+            if(testLog.indexOf('scored "16"') > -1)   {
+                //db2.matches.insert({user: user , match: obj});
+                jsonfile.writeFile(fileOut, obj, function (err) {
+                    console.error(err)
+                })
+            }
         };
     }
 };
--- backend/nodejs/test/testing.js
+++ backend/nodejs/test/testing.js
@@ -3,6 +3,9 @@ var dgram = require('dgram'),
 
 var cmd = require('../js/cmd');
 
+var mongojs = require('mongojs');
+var db2 = mongojs('matches', ['matches']);
+
 server.on('listening', function () {
     var address = server.address();
     console.log('UDP Server listening ' + address.address + ':' + address.port);
@@ -20,6 +23,14 @@ var fileIn = 'data/dataIn.json';
 
 var players;
 
+var date = new Date();
+
+var dataStr = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' at ' +  date.getHours() + ':' + date.getMinutes();
+
+var map;
+
+var user = "sally";
+
 var obj = [];
 
 var init = function () {
@@ -27,13 +38,17 @@ var init = function () {
     var length = Object.keys(players).length;
     obj = [];
     var index;
-    for (index = 0; index < length; ++index) {
-        obj.push({
-            id: players[index].fullSteamId,
-            name: players[index].name,
-            kills: 0,
-            deaths: 0
-        });
+    for (index = 0; index < length +1; ++index) {
+        if(index < length){
+            obj.push({
+                id: players[index].fullSteamId,
+                name: players[index].name,
+                kills: 0,
+                deaths: 0
+            });
+        } else
+            obj.push({map : map,
+            date: dataStr})
     }
 };
 
@@ -41,7 +56,8 @@ server.on('message', function (message, rinfo) {
     var msg = message.toString('ascii');
     console.log(msg);
     if (msg.indexOf("Match_Start") > -1) {
-        cmd.serverAccess();                     // this is for full test
+        map = msg.substr(msg.indexOf("Match_Start")+17, msg.length - (msg.indexOf("Match_Start")+19));
+        //cmd.serverAccess();                     // this is for full test
         console.log("start match");
         init();
         console.log("writing log(match start)");
@@ -59,12 +75,12 @@ server.on('message', function (message, rinfo) {
  console.log(Object.keys(obj).length);
  */
 var write = function (testLog) {
-    var length = Object.keys(obj).length; // == 2
+    var length = Object.keys(obj).length;
 
     if (testLog.indexOf("killed") > -1) {
         var index;
         var count = 0;
-        for (index = 0; index < length; ++index) {
+        for (index = 0; index < length-1; ++index) {
 
             var steam = index; // steamid#
             if (count == 2) break;              // we need to count only 2 times (kill and dead) and another circles doesn't matter
@@ -91,14 +107,14 @@ var write = function (testLog) {
         }
     } else if (testLog.indexOf("Match_Start") > -1) {
             var index2;
-            for (index2 = 0; index2 < length; ++index2) {
+            for (index2 = 0; index2 < length-1; ++index2) {
                 var steam2 = index2;
                 obj[steam2].kills = 0;
                 obj[steam2].deaths = 0;
             }
         } else if (testLog.indexOf("suicide") > -1) {
         var i;
-        for (i = 0; i < length; ++i) {
+        for (i = 0; i < length-1; ++i) {
 
             var steam = i; // steamid#
             if (testLog.indexOf(obj[steam].id) > -1) {
@@ -111,8 +127,16 @@ var write = function (testLog) {
 
     //console.log(obj);
 
-    if (testLog.indexOf('scored "16"') > -1)           // write dataOut file only if match end
+/*    if (testLog.indexOf('scored "16"') > -1)           // write dataOut file only if match end
         jsonfile.writeFile(fileOut, obj, function (err) {
             console.error(err)
-        });
+        });*/
+
+    if(testLog.indexOf('scored "16"') > -1)   {
+        //db2.matches.insert({user: user , match: obj});
+        jsonfile.writeFile(fileOut, obj, function (err) {
+            console.error(err)
+        })
+    }
+
 };
